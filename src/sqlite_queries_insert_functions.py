c6f6fac5f1eebeec336d9a9e6183eaad8f22ae66
Michael Frood <132622476+Frood-for-Thought@users.noreply.github.com>
2023-10-31 14:16:28 -0400

    Update sqlite_queries_insert_functions.py

--- src/sqlite_queries_insert_functions.py
+++ src/sqlite_queries_insert_functions.py
@@ -2,6 +2,7 @@ import sqlite3 as sql
 import pandas as pd
 
 class sqlite_con_queries_inserts:
+    # Cursor is now created with class
     def __init__(self, path, queries):
         self.path = path
         self.connection = self.create_connection()
@@ -34,3 +35,16 @@ class sqlite_con_queries_inserts:
             print("An error occurred:", e)
             # Rollback the transaction in case of error
             self.cursor.connection.rollback()
+    
+    # Inerts static function
+    @staticmethod
+    def execute_inserts(table, query, path):
+        # Convert DataFrame rows to list of dictionaries
+        my_list = table.to_dict(orient='records')
+        # Iterate through the list of dictionaries and insert data
+        for row in my_list:
+            try:
+                sqlite_con_queries_inserts(path, query).execute_query(data=row, print_message=False)
+            except sql.Error as e:
+                print("An error occurred:", e)
+        print("Insert query executed successfully")
